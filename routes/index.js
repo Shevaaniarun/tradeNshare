1ddcb6f1290694c611cbedac2442b92d42c18a2e
Shevaani <144267473+Shevaaniarun@users.noreply.github.com>
2026-03-31 12:39:41 +0530

    Changed case to avoid errors

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { renderHome } = require('../controllers/indexController');
+const { renderHome } = require('../controllers/indexcontroller');
 
 router.get('/', renderHome);
 
